c83a309aba99d15175dce9012005f98bbda5ed9f
JackY <jack.yujia.yuan@gmail.com>
2019-08-29 17:24:21 +1000

    Save Person using Redux.

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {Component}from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+  state = {
+    name: '',
+    age: '',
+}
 
-export default addPerson;
+  onNameChange = (e) => {
+      this.setState({
+        name: e.target.value,
+      })
+  }
+  onAgeChange = (e) => {
+      this.setState({
+        age: e.target.value,
+      })
+  }
+  render() {
+    return (
+      <div className="AddPerson">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={this.onNameChange}
+          value = {this.state.name}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={this.onAgeChange}
+          value={this.state.age}
+        />
+        <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+      </div>
+    )
+  }
+}
+
+export default AddPerson;
